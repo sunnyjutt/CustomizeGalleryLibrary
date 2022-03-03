415ecba043c022f72b67ffe3b55558a301f9cfe8
OZI\awaissarwar <awais.sarwar.apps>
2022-03-03 12:54:59 +0500

    open gallery

--- customgallerylibrary/src/main/java/com/example/customgallerylibrary/HomeActivity.kt
+++ customgallerylibrary/src/main/java/com/example/customgallerylibrary/HomeActivity.kt
@@ -1,11 +1,62 @@
 package com.example.customgallerylibrary
 
-import androidx.appcompat.app.AppCompatActivity
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.os.Build
+import android.os.Build.VERSION.SDK_INT
 import android.os.Bundle
+import android.os.Environment
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+
 
 class HomeActivity : AppCompatActivity() {
+
+    val REQUEST_CODE = 123
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+
+        if (SDK_INT >= Build.VERSION_CODES.R) {
+            Log.d("myz", "" + SDK_INT)
+            if (!Environment.isExternalStorageManager()) {
+                ActivityCompat.requestPermissions(
+                    this, arrayOf(
+                        Manifest.permission.READ_EXTERNAL_STORAGE,
+                        Manifest.permission.MANAGE_EXTERNAL_STORAGE
+                    ), 1
+                ) //permission request code is just an int
+            }
+        } else {
+            if (ActivityCompat.checkSelfPermission(
+                    this,
+                    Manifest.permission.READ_EXTERNAL_STORAGE
+                ) != PackageManager.PERMISSION_GRANTED || ActivityCompat.checkSelfPermission(
+                    this,
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE
+                ) != PackageManager.PERMISSION_GRANTED
+            ) {
+                ActivityCompat.requestPermissions(
+                    this,
+                    arrayOf(
+                        Manifest.permission.READ_EXTERNAL_STORAGE,
+                        Manifest.permission.WRITE_EXTERNAL_STORAGE
+                    ),
+                    1
+                )
+            }
+        }
+        openGalleryForImage()
+
+    }
+
+     fun openGalleryForImage() {
+        val intent = Intent(Intent.ACTION_PICK)
+        intent.type = "image/*"
+        startActivityForResult(intent, REQUEST_CODE)
     }
 }
